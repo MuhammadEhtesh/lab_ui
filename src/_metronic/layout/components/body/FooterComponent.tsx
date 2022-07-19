8c7fd93b3ede8f5cb606e74993f0ba4eafe7c4ac
muhammadehtesh <muhammadehteshambhatti@gmail.com>
2022-07-19 20:09:52 +0500

    Footer Component with mui

--- src/_metronic/layout/components/body/FooterComponent.tsx
+++ src/_metronic/layout/components/body/FooterComponent.tsx
@@ -1,50 +1,54 @@
+import {Container, Grid, Box, Link} from '@mui/material'
+
 export const FooterComponent = () => {
   return (
-    <div className='main-footer mt-4'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            <h4>Google Map</h4>
-          </div>
-          <div className='col'>
-            <h4>Company Info</h4>
-            <ul>
-              <li>
-                <i className='fa fa-map-marker text-black'></i>
-                <a target='_blank' href='#'>
-                  601 E Atlantic Blvd. Pompano Beach, FL 33060{' '}
-                </a>
-              </li>
-              <li>
-                <i className='fa fa-phone text-black'></i>
-                <a target='_blank' href='#'>
-                  954-333-8149{' '}
-                </a>
-              </li>
-              <li>
-                <i className='fa fa-envelope text-black'></i>
-                <a target='_blank' href='#'>
-                  customerservice@aemlinc.com{' '}
-                </a>
-              </li>
-              <li>
-                <i className='bi bi-clock text-black'></i>
-                <a target='_blank' href='#'>
-                  9:00 am to 5:00 pm EST, Monday to Friday{' '}
-                </a>
-              </li>
-              <li>
-                <a target='_blank' href='#'>
-                  <i className='bi bi-facebook'></i>{' '}
-                </a>
-                <a target='_blank' href='#'>
-                  <i className='bi bi-linkedin'></i>{' '}
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Box px={{xs: 3, sm: 10}} py={{xs: 3, sm: 10}}>
+      <Container maxWidth='lg'>
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Box>
+              <h4>Google Maps</h4>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box>
+              <h4>Company Info</h4>
+            </Box>
+            <Box>
+              <Link href='/' color='inherit'>
+                <i className='fa fa-map-marker'></i>
+                601 E Atlantic Blvd. Pompano Beach, FL 33060
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color='inherit'>
+                <i className='fa fa-phone'></i>
+                954-333-8149
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color='inherit'>
+                <i className='fa fa-envelope'></i>
+                customerservice@aemlinc.com
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color='inherit'>
+                <i className='bi bi-clock'></i>
+                9:00 am to 5:00 pm EST, Monday to Friday
+              </Link>
+            </Box>
+            <Box>
+              <Link href='/' color='inherit'>
+                <i className='bi bi-facebook'></i>
+              </Link>
+              <Link href='/' color='inherit'>
+                <i className='bi bi-linkedin'></i>
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
